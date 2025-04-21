80c1a864c006bfb9e183491577137e8040149f7a
Matheus Fillipe <matheusfillipeag@gmail.com>
2025-04-21 19:54:21 +0200

    add tests to workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "/",
+  plugins: [react()],
+  base: "/",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts"
+  }
 });
