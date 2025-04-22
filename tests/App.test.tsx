ef90d888e96672adaa35384b8901eacf3974cef3
Matheus Fillipe <matheusfillipeag@gmail.com>
2025-04-22 11:52:22 +0200

    Add preview comment to PR

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -6,10 +6,20 @@ import { describe, expect, it } from "vitest";
 
 describe("App", () => {
   describe("When loading app", () => {
-    it("Add server should be visible", async () => {
+    it("Can add servers", async () => {
       render(<App />);
       user.click(screen.getByTestId("server-list-options-button"));
       expect(await screen.findByText(/Add Server/i)).toBeInTheDocument();
+      user.click(screen.getByText(/Add Server/i));
+      expect(await screen.findByText(/Add IRC Server/i)).toBeInTheDocument();
+      user.type(screen.getByPlaceholderText(/Server/i), "Test Server");
+      user.type(
+        screen.getByPlaceholderText(/irc\.example\.com/i),
+        "irc.mattf.one",
+      );
+      user.type(screen.getByPlaceholderText(/443/i), "443");
+      user.click(screen.getByText(/Connect/i));
+      // expect(await screen.findByText(/Online/)).toBeInTheDocument();
     });
   });
 });
