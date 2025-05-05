d8199b4d8123753e7289a0800682930c739477cc
Matheus Fillipe <matheusfillipeag@gmail.com>
2025-05-05 23:35:05 +0200

    sugestions

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,10 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./tests/setup.ts",
     include: ["tests/**/*.test.tsx", "tests/**/*.test.ts"],
+  },
+  define: {
+    '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+    '__APP_NAME__': JSON.stringify(process.env.npm_package_name),
+    '__APP_DESCRIPTION__': JSON.stringify(process.env.npm_package_description),
   }
 });
