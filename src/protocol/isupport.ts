7f940a0d5b9e5d6829bb812e58d213d5017b669b
Valerie Liu <v.a.pond@outlook.com>
2025-05-17 11:37:24 +0100

    Move ISUPPORT to new file

--- src/protocol/isupport.ts
+++ src/protocol/isupport.ts
@@ -1,28 +1,28 @@
 import type { IRCClient } from "../lib/ircClient";
 import type AppState from "../store/";
-import type { ISupportEvent } from "../types/";
+import type { ISupportEvent, Server } from "../types/";
 
 export function registerISupportHandler(
   ircClient: IRCClient,
   useStore: typeof AppState,
 ) {
   ircClient.on("ISUPPORT", ({ serverId, capabilities }: ISupportEvent) => {
-    // const paramsArray = capabilities;
-    // console.log(capabilities);
-    // for (let i = 0; i < paramsArray.length; i++) {
-    //   /* Favicon checking */
-    //   if (paramsArray[i].startsWith("FAVICON=")) {
-    //     const favicon = paramsArray[i].substring(8);
-    //     useStore.setState((state: AppState) => {
-    //       const updatedServers = state.servers.map((server: Server) => {
-    //         if (server.id === serverId) {
-    //           return { ...server, icon: favicon };
-    //         }
-    //         return server;
-    //       });
-    //       return { servers: updatedServers };
-    //     });
-    //   }
-    // }
+    const paramsArray = capabilities;
+    console.log(capabilities);
+    for (let i = 0; i < paramsArray.length; i++) {
+      /* Favicon checking */
+      if (paramsArray[i].startsWith("FAVICON=")) {
+        const favicon = paramsArray[i].substring(8);
+        useStore.setState((state) => {
+          const updatedServers = state.servers.map((server: Server) => {
+            if (server.id === serverId) {
+              return { ...server, icon: favicon };
+            }
+            return server;
+          });
+          return { servers: updatedServers };
+        });
+      }
+    }
   });
 }
