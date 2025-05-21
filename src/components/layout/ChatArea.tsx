7a0007dceed241a4c5656e53aa9b7732e8d6dd11
Valerie Liu <v.a.pond@outlook.com>
2025-05-21 05:06:59 +0100

    You can now view and select colors & bold/italic/underline...more to come

--- src/components/layout/ChatArea.tsx
+++ src/components/layout/ChatArea.tsx
@@ -8,7 +8,6 @@ import {
   FaBell,
   FaChevronLeft,
   FaChevronRight,
-  FaGift,
   FaGrinAlt,
   FaHashtag,
   FaPenAlt, // Added
@@ -206,6 +205,9 @@ export const ChatArea: React.FC<{
   const [localReplyTo, setLocalReplyTo] = useState<MessageType | null>(null);
   const [messageText, setMessageText] = useState("");
   const [isEmojiSelectorOpen, setIsEmojiSelectorOpen] = useState(false);
+  const [isColorPickerOpen, setIsColorPickerOpen] = useState(false);
+  const [selectedColor, setSelectedColor] = useState("#ffffff");
+  const [selectedFormatting, setSelectedFormatting] = useState<string[]>([]);
   const [isScrolledUp, setIsScrolledUp] = useState(false);
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const messagesContainerRef = useRef<HTMLDivElement>(null);
@@ -273,13 +275,17 @@ export const ChatArea: React.FC<{
       container.removeEventListener("scroll", checkIfScrolledToBottom);
   }, []);
 
+  const getColorCode = (color: string): string => {
+    const index = IRC_COLORS.indexOf(color);
+    return index !== -1 ? `\x03${index}` : ""; // Return \x03 followed by the index, or an empty string if not found
+  };
+
   const handleSendMessage = () => {
     if (messageText.trim() === "") return;
     scrollDown();
     if (selectedServerId && selectedChannelId) {
       if (messageText.startsWith("/")) {
         // Handle command
-        // TODO: Implement proper command registry
         const command = messageText.substring(1).trim();
         const [commandName, ...args] = command.split(" ");
         if (commandName === "nick") {
@@ -309,16 +315,32 @@ export const ChatArea: React.FC<{
             `PRIVMSG ${selectedChannel ? selectedChannel.name : ""} :\u0001ACTION ${actionMessage}\u0001`,
           );
         } else {
-          // Handle other commands
           ircClient.sendRaw(
             selectedServerId,
             `${commandName} :${args.join(" ")}`,
           );
         }
       } else {
+        const colorCode = getColorCode(selectedColor); // Get the IRC color code
+
+        // Apply formatting codes
+        let formattedText = messageText;
+        if (selectedFormatting.includes("bold")) {
+          formattedText = `\x02${formattedText}\x02`;
+        }
+        if (selectedFormatting.includes("italic")) {
+          formattedText = `\x1D${formattedText}\x1D`;
+        }
+        if (selectedFormatting.includes("underline")) {
+          formattedText = `\x1F${formattedText}\x1F`;
+        }
+
+        // Prepend the color code
+        formattedText = `${colorCode}${formattedText}`;
+
         ircClient.sendRaw(
           selectedServerId,
-          `${localReplyTo ? `@+reply=${localReplyTo.id};` : ""} PRIVMSG ${selectedChannel?.name ?? ""} :${messageText}`,
+          `${localReplyTo ? `@+reply=${localReplyTo.id};` : ""} PRIVMSG ${selectedChannel?.name ?? ""} :${formattedText}`,
         );
       }
       setMessageText("");
@@ -367,6 +389,20 @@ export const ChatArea: React.FC<{
   const handleEmojiSelect = (emoji: string) => {
     setMessageText((prev) => prev + emoji);
     setIsEmojiSelectorOpen(false);
+  };
+
+  const handleColorSelect = (color: string, formatting: string[]) => {
+    setSelectedColor(color);
+    setSelectedFormatting(formatting);
+    setIsColorPickerOpen(false);
+  };
+
+  const toggleFormatting = (format: string) => {
+    setSelectedFormatting((prev) =>
+      prev.includes(format)
+        ? prev.filter((f) => f !== format)
+        : [...prev, format],
+    );
   };
 
   const isNarrowView = useMediaQuery();
@@ -540,6 +576,18 @@ export const ChatArea: React.FC<{
               onKeyDown={handleKeyDown}
               placeholder={`Message #${selectedChannel.name.replace(/^#/, "")}`}
               className="bg-transparent border-none outline-none py-3 flex-grow text-discord-text-normal"
+              style={{
+                color: selectedColor,
+                fontWeight: selectedFormatting.includes("bold")
+                  ? "bold"
+                  : "normal",
+                fontStyle: selectedFormatting.includes("italic")
+                  ? "italic"
+                  : "normal",
+                textDecoration: selectedFormatting.includes("underline")
+                  ? "underline"
+                  : "none",
+              }}
             />
             <button
               className="px-3 text-discord-text-muted hover:text-discord-text-normal"
@@ -547,8 +595,17 @@ export const ChatArea: React.FC<{
             >
               <FaGrinAlt />
             </button>
-            <button className="px-3 text-discord-text-muted hover:text-discord-text-normal">
-              <FaGift />
+            <button
+              className="px-3 text-discord-text-muted hover:text-discord-text-normal"
+              onClick={() => setIsColorPickerOpen((prev) => !prev)}
+            >
+              <div
+                className="w-4 h-4 rounded-full border-dark-2 -500"
+                style={{
+                  backgroundColor:
+                    selectedColor === "inherit" ? "transparent" : selectedColor,
+                }}
+              />
             </button>
             <button className="px-3 text-discord-text-muted hover:text-discord-text-normal">
               <FaAt />
@@ -561,8 +618,202 @@ export const ChatArea: React.FC<{
               onClose={() => setIsEmojiSelectorOpen(false)}
             />
           )}
+
+          {isColorPickerOpen && (
+            <ColorPicker
+              onSelect={(color) => setSelectedColor(color)}
+              onClose={() => setIsColorPickerOpen(false)}
+              selectedFormatting={selectedFormatting}
+              toggleFormatting={toggleFormatting}
+            />
+          )}
         </div>
       )}
+    </div>
+  );
+};
+
+const IRC_COLORS = [
+  "#FFFFFF",
+  "#000000",
+  "#00009D",
+  "#009300",
+  "#FF0000",
+  "#7F0000",
+  "#9C009C",
+  "#FC7F00",
+  "#FFFF00",
+  "#00FC00",
+  "#009393",
+  "#00FFFF",
+  "#0000FC",
+  "#FF00FF",
+  "#7F7F7F",
+  "#D2D2D2",
+  "#470000",
+  "#472100",
+  "#474700",
+  "#324700",
+  "#004700",
+  "#00472C",
+  "#004747",
+  "#002747",
+  "#000047",
+  "#2E0047",
+  "#470047",
+  "#47002A",
+  "#740000",
+  "#743A00",
+  "#747400",
+  "#517400",
+  "#007400",
+  "#007449",
+  "#007474",
+  "#004074",
+  "#000074",
+  "#4B0074",
+  "#740074",
+  "#740045",
+  "#B50000",
+  "#B56300",
+  "#B5B500",
+  "#7DB500",
+  "#00B500",
+  "#00B571",
+  "#00B5B5",
+  "#0063B5",
+  "#0000B5",
+  "#7500B5",
+  "#B500B5",
+  "#B5006B",
+  "#FF000B",
+  "#FF8C00",
+  "#FFFF0B",
+  "#B2FF00",
+  "#00FF00",
+  "#00FFA0",
+  "#00FFFB",
+  "#008CFF",
+  "#0000FF",
+  "#A500FF",
+  "#FF00FB",
+  "#FF0098",
+  "#FF5959",
+  "#FFB459",
+  "#FFFF71",
+  "#CFFF60",
+  "#6FFF6F",
+  "#65FFC9",
+  "#6DFFFF",
+  "#59B4FF",
+  "#5959FF",
+  "#C459FF",
+  "#FF66FF",
+  "#FF59BC",
+  "#FF9C9C",
+  "#FFD39C",
+  "#FFFF9C",
+  "#E2FF9C",
+  "#9CFF9C",
+  "#9CFFDB",
+  "#9CFFFF",
+  "#9CD3FF",
+  "#9C9CFF",
+  "#DC9CFF",
+  "#FF9CFF",
+  "#FF94D3",
+  "#00000A",
+  "#131313",
+  "#282828",
+  "#363636",
+  "#4D4D4D",
+  "#656565",
+  "#818181",
+  "#9F9F9F",
+  "#BCBCBC",
+  "#E2E2E2",
+  "#FFFFF0",
+  "inherit",
+];
+
+const ColorPicker: React.FC<{
+  onSelect: (color: string, formatting: string[]) => void;
+  onClose: () => void;
+  selectedFormatting: string[];
+  toggleFormatting: (format: string) => void;
+}> = ({ onSelect, onClose, selectedFormatting, toggleFormatting }) => {
+  const [selectedColor, setSelectedColor] = useState<string | null>(null);
+
+  return (
+    <div className="absolute bottom-16 right-4 z-50 bg-discord-dark-300 p-4 rounded shadow-lg">
+      <div className="grid grid-cols-8 gap-2 mb-4">
+        {IRC_COLORS.map((color, index) => {
+          const uniqueKey = `obsidian-${color}-${index}`; // Copy the index to another variable
+          const isSelected = selectedColor === color;
+
+          return (
+            <button
+              key={uniqueKey} // Use the new variable for the key
+              className={`w-6 h-6 rounded border-2 flex items-center justify-center ${
+                isSelected
+                  ? "border-dark-500 shadow-md shadow-black-500"
+                  : "border-dark-500"
+              }`}
+              style={{
+                backgroundColor: color === "inherit" ? "transparent" : color,
+              }}
+              onClick={() => {
+                setSelectedColor(color);
+                onSelect(color, selectedFormatting);
+              }}
+            >
+              {color === "inherit" && (
+                <span className="text-xs text-purple-500 font-bold">
+                  <FaTimes />
+                </span>
+              )}
+            </button>
+          );
+        })}
+      </div>
+      <div className="flex justify-between mb-4">
+        <button
+          className={`px-2 py-1 rounded ${
+            selectedFormatting.includes("bold")
+              ? "bg-discord-dark-200"
+              : "bg-discord-dark-400"
+          }`}
+          onClick={() => toggleFormatting("bold")}
+        >
+          Bold
+        </button>
+        <button
+          className={`px-2 py-1 rounded ${
+            selectedFormatting.includes("italic")
+              ? "bg-discord-dark-200"
+              : "bg-discord-dark-400"
+          }`}
+          onClick={() => toggleFormatting("italic")}
+        >
+          Italic
+        </button>
+        <button
+          className={`px-2 py-1 rounded ${
+            selectedFormatting.includes("underline")
+              ? "bg-discord-dark-200"
+              : "bg-discord-dark-400"
+          }`}
+          onClick={() => toggleFormatting("underline")}
+        >
+          Underline
+        </button>
+      </div>
+      <button
+        onClick={onClose}
+        className="w-full bg-discord-dark-400 hover:bg-discord-dark-200 text-white py-1 rounded"
+      >
+        Close
+      </button>
     </div>
   );
 };
